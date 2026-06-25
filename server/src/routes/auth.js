17a05e4ee4f75088c7f2f5bfc35891b7e5d62240
Sonu99kr <krsonu.work@gmail.com>
2026-06-25 18:08:04 +0530

    same site option changed

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -9,7 +9,7 @@ const SALT_ROUNDS = 12;
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
